ca5bd529ab2519504475ece1d83dfbaaaf7c66a3
unknown <zoharcor@gmail.com>
2020-08-16 22:11:07 +0300

    Adding login and register  routes

--- giveMeInsurace-client/src/app/app-routing.module.ts
+++ giveMeInsurace-client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+      RouterModule.forRoot(routes)
+    ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- giveMeInsurace-client/src/app/app.module.ts
+++ giveMeInsurace-client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
